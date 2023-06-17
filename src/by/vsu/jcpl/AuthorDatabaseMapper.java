2bfa0e725bcbf8a7a652a60d3ae854b57ccb199d
Sergey Yermochenko <yermochenko@gmail.com>
2023-06-17 14:05:46 +0300

    Updating of information about author was implemented

--- src/by/vsu/jcpl/AuthorDatabaseMapper.java
+++ src/by/vsu/jcpl/AuthorDatabaseMapper.java
@@ -21,18 +21,28 @@ public class AuthorDatabaseMapper {
 			resultSet = statement.executeQuery(sql);
 			List<Author> authors = new ArrayList<>();
 			while(resultSet.next()) {
-				Author author = new Author();
-				author.setId(resultSet.getInt("id"));
-				author.setSurname(resultSet.getString("surname"));
-				author.setName(resultSet.getString("name"));
-				author.setBirthYear(resultSet.getInt("birth_year"));
-				Integer deathYear = resultSet.getInt("death_year");
-				if(!resultSet.wasNull()) {
-					author.setDeathYear(deathYear);
-				}
-				authors.add(author);
+				authors.add(parseResultSet(resultSet));
 			}
 			return authors;
+		} finally {
+			try { Objects.requireNonNull(resultSet).close(); } catch(Exception ignored) {}
+			try { Objects.requireNonNull(statement).close(); } catch(Exception ignored) {}
+		}
+	}
+
+	public Author readById(Integer id) throws SQLException {
+		String sql = "SELECT \"id\", \"surname\", \"name\", \"birth_year\", \"death_year\" FROM \"author\" WHERE \"id\" = ?";
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			statement = connection.prepareStatement(sql);
+			statement.setInt(1, id);
+			resultSet = statement.executeQuery();
+			if(resultSet.next()) {
+				return parseResultSet(resultSet);
+			} else {
+				return null;
+			}
 		} finally {
 			try { Objects.requireNonNull(resultSet).close(); } catch(Exception ignored) {}
 			try { Objects.requireNonNull(statement).close(); } catch(Exception ignored) {}
@@ -45,14 +55,7 @@ public class AuthorDatabaseMapper {
 		ResultSet resultSet = null;
 		try {
 			statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			statement.setString(1, author.getName());
-			statement.setString(2, author.getSurname());
-			statement.setInt(3, author.getBirthYear());
-			if(author.getDeathYear() != null) {
-				statement.setInt(4, author.getDeathYear());
-			} else {
-				statement.setNull(4, Types.INTEGER);
-			}
+			fillStatement(statement, author);
 			statement.executeUpdate();
 			resultSet = statement.getGeneratedKeys(); // receiving of all generated primary keys
 			resultSet.next();
@@ -61,5 +64,42 @@ public class AuthorDatabaseMapper {
 			try { Objects.requireNonNull(resultSet).close(); } catch(Exception ignored) {}
 			try { Objects.requireNonNull(statement).close(); } catch(Exception ignored) {}
 		}
+	}
+
+	public void update(Author author) throws SQLException {
+		String sql = "UPDATE \"author\" SET \"name\" = ?, \"surname\" = ?, \"birth_year\" = ?, \"death_year\" = ? WHERE \"id\" = ?";
+		PreparedStatement statement = null;
+		try {
+			statement = connection.prepareStatement(sql);
+			fillStatement(statement, author);
+			statement.setInt(5, author.getId());
+			statement.executeUpdate();
+		} finally {
+			try { Objects.requireNonNull(statement).close(); } catch(Exception ignored) {}
+		}
+	}
+
+	private void fillStatement(PreparedStatement statement, Author author) throws SQLException {
+		statement.setString(1, author.getName());
+		statement.setString(2, author.getSurname());
+		statement.setInt(3, author.getBirthYear());
+		if(author.getDeathYear() != null) {
+			statement.setInt(4, author.getDeathYear());
+		} else {
+			statement.setNull(4, Types.INTEGER);
+		}
+	}
+
+	private Author parseResultSet(ResultSet resultSet) throws SQLException {
+		Author author = new Author();
+		author.setId(resultSet.getInt("id"));
+		author.setSurname(resultSet.getString("surname"));
+		author.setName(resultSet.getString("name"));
+		author.setBirthYear(resultSet.getInt("birth_year"));
+		Integer deathYear = resultSet.getInt("death_year");
+		if(!resultSet.wasNull()) {
+			author.setDeathYear(deathYear);
+		}
+		return author;
 	}
 }
--- src/by/vsu/jcpl/Main.java
+++ src/by/vsu/jcpl/Main.java
@@ -13,13 +13,14 @@ public class Main {
 			Scanner console = new Scanner(System.in);
 			boolean work = true;
 			while(work) {
-				System.out.println("+----------------------------+");
-				System.out.println("|            MENU            |");
-				System.out.println("+----------------------------+");
-				System.out.println("| 1) Reading of authors list |");
-				System.out.println("| 2) Adding of new author    |");
-				System.out.println("| 3) Exit                    |");
-				System.out.println("+----------------------------+");
+				System.out.println("+--------------------------------------+");
+				System.out.println("|                 MENU                 |");
+				System.out.println("+--------------------------------------+");
+				System.out.println("| 1) Reading of authors list           |");
+				System.out.println("| 2) Adding of new author              |");
+				System.out.println("| 3) Updating information about author |");
+				System.out.println("| 4) Exit                              |");
+				System.out.println("+--------------------------------------+");
 				System.out.print("\nEnter menu item number: ");
 				try {
 					int menuItem = Integer.parseInt(console.nextLine());
@@ -76,6 +77,72 @@ public class Main {
 							System.out.printf("Author successfully added with identifier [%04d]\n", id);
 						}
 						case 3 -> {
+							System.out.println("\n==<[ UPDATING INFORMATION ABOUT AUTHOR ]>==\n");
+							System.out.print("Enter author's identifier (ID): ");
+							int id;
+							try {
+								id = Integer.parseInt(console.nextLine());
+							} catch(NumberFormatException e) {
+								throw new EntityValidationException("Identifier should be integer");
+							}
+							Author author = new AuthorDatabaseMapper(connection).readById(id);
+							boolean isChanged = false;
+							System.out.printf("Author's name: %s\n", author.getName());
+							System.out.print("Enter new name (or press \"Enter\" to leave existing name): ");
+							String name = console.nextLine();
+							if(!name.isBlank()) {
+								author.setName(name);
+								isChanged = true;
+							}
+							System.out.printf("Author's surname: %s\n", author.getSurname());
+							System.out.print("Enter new surname (or press \"Enter\" to leave existing surname): ");
+							String surname = console.nextLine();
+							if(!surname.isBlank()) {
+								author.setSurname(surname);
+								isChanged = true;
+							}
+							System.out.printf("Author's birth year: %d\n", author.getBirthYear());
+							System.out.print("Enter new birth year (or press \"Enter\" lo leave existing birth year): ");
+							String birthYear = console.nextLine();
+							if(!birthYear.isBlank()) {
+								try {
+									author.setBirthYear(Integer.valueOf(birthYear));
+								} catch(NumberFormatException e) {
+									throw new EntityValidationException("Birth year should be integer");
+								}
+								isChanged = true;
+							}
+							System.out.print("Author's death year: ");
+							if(author.getDeathYear() != null) {
+								System.out.printf("%d\n", author.getDeathYear());
+							} else {
+								System.out.println("---");
+							}
+							System.out.print("Enter new death year (or press \"Enter\" to leave existing death year, or print \"-\" if there is need to blank death year): ");
+							String deathYear = console.nextLine();
+							if(!deathYear.isBlank()) {
+								if("-".equals(deathYear)) {
+									author.setDeathYear(null);
+								} else {
+									try {
+										author.setDeathYear(Integer.valueOf(deathYear));
+									} catch(NumberFormatException e) {
+										throw new EntityValidationException("Death year should be integer");
+									}
+								}
+								isChanged = true;
+							}
+							if(author.getDeathYear() != null && author.getBirthYear() > author.getDeathYear()) {
+								throw new EntityValidationException("Birth year should be earlier than death year");
+							}
+							if(isChanged) {
+								new AuthorDatabaseMapper(connection).update(author);
+								System.out.println("Information about author was successfully updated");
+							} else {
+								System.out.println("Nothing changed");
+							}
+						}
+						case 4 -> {
 							System.out.print("\nDo you want to exit? (y/n): ");
 							if(Set.of("y", "yes").contains(console.nextLine().trim().toLowerCase())) {
 								System.out.println("Good bye");
